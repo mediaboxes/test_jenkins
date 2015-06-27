1cf85e470b576059e642edfd634e418ee3c00541
mediaboxes <mediaboxes@gmail.com>
2015-06-27 23:25:43 +0900

    test4

--- app/src/main/java/tokyo/mediaboxes/test_jenkins/MainActivity.java
+++ app/src/main/java/tokyo/mediaboxes/test_jenkins/MainActivity.java
@@ -28,15 +28,11 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        return false;
-
-
-
         //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
