f517cc9955f77f35d959e04f76b46fc4291be886
mediaboxes <mediaboxes@gmail.com>
2015-06-30 02:06:40 +0900

    FindBugs,JaCoCo のレポート出力設定

---
+++ app/src/test/tokyo/mediaboxes/MainActivityTest.java
@@ -0,0 +1,19 @@
+package tokyo.mediaboxes.test_jenkins;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mono on 2015/06/29.
+ */
+public class MainActivityTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+}
